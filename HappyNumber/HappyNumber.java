db0d67ba206e9fd0fe8752dc26ff48acc0535fd2
purimetlamuralikrishna <muralikrishna70321@gmail.com>
2022-09-01 20:16:19 +0530

    HappyNumber

---
+++ HappyNumber/HappyNumber.java
@@ -0,0 +1,33 @@
+
+class HappyNumber{
+	public static void main(String[] args){
+	
+		int num = 19;
+		String ans = isNumberHappy(num)?"Happy":"Not Happy";
+		System.out.println(ans);
+	}
+
+	public static boolean isNumberHappy(int num){
+	
+		if(num==1||num==7){
+			return true;
+		}
+	
+		if(num<10){
+			return false;
+		}
+		
+		int sum = 0;
+		while(num!=0){
+			
+			int i=num%10;
+			sum += i*i;
+			
+			num = num/10;
+		}
+		System.out.println(sum);
+		
+		return isNumberHappy(sum);
+		
+	}
+}
